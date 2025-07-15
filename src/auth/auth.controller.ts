001b66ab859ca4390179cf954700acde163c1892
rsirm <roman.sirmanov@bd.com>
2025-07-15 16:45:42 +0300

    feat: auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -9,5 +9,10 @@ export class AuthController {
   @Post('/signup')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
     return this.authService.SignUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.SignIn(authCredentialsDto);
   }
 }
